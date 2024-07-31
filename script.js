01b0fbde42c3decc8028c74c48c3d9c57706dd41
feeags96 <153056979+feeags96@users.noreply.github.com>
2024-07-31 16:55:24 -0300

    Ajustes.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    prevButton.addEventListener('click', () => {
+     prevButton.addEventListener('click', () => {
         currentIndex = (currentIndex === 0) ? items.length - 1 : currentIndex - 1;
         showItem(currentIndex);
     });
@@ -56,160 +56,160 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function sanitizeString(str) {
-    var sanitizedStr = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-    sanitizedStr = sanitizedStr.replace(/\s+/g, '-');   
-    sanitizedStr = sanitizedStr.replace(/[^a-z0-9-]/g, '');
-    return sanitizedStr;
-}
+// function sanitizeString(str) {
+//     var sanitizedStr = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+//     sanitizedStr = sanitizedStr.replace(/\s+/g, '-');   
+//     sanitizedStr = sanitizedStr.replace(/[^a-z0-9-]/g, '');
+//     return sanitizedStr;
+// }
 
-document.getElementById("back-to-top").addEventListener("click", function () {
-    gtag('event', 'clique', {
-        'custom_section': 'home',
-        'custom_type': 'botao',
-        'custom_title': 'retornar-topo'
-    });
-});
+// document.getElementById("back-to-top").addEventListener("click", function () {
+//     gtag('event', 'clique', {
+//         'custom_section': 'home',
+//         'custom_type': 'botao',
+//         'custom_title': 'retornar-topo'
+//     });
+// });
 
-    window.addEventListener('DOMContentLoaded', function() {
-        let currentIndex = 0;
-        const items = document.querySelectorAll('.carousel-item');
-        const totalItems = items.length;
+//     window.addEventListener('DOMContentLoaded', function() {
+//         let currentIndex = 0;
+//         const items = document.querySelectorAll('.carousel-item');
+//         const totalItems = items.length;
 
-        function updateCarousel(index) {
-            items.forEach((item, i) => {
-                item.classList.toggle('active', i === index);
-            });
-            gtag('event', 'clique', {
-                'custom_section': 'view-banner',
-                'custom_type': 'carousel',
-                'custom_title': `banner-${index + 1}`
-            });
-        }
+//         function updateCarousel(index) {
+//             items.forEach((item, i) => {
+//                 item.classList.toggle('active', i === index);
+//             });
+//             gtag('event', 'clique', {
+//                 'custom_section': 'view-banner',
+//                 'custom_type': 'carousel',
+//                 'custom_title': `banner-${index + 1}`
+//             });
+//         }
 
-        document.getElementById('voltar').addEventListener('click', function() {
-            currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
-            updateCarousel(currentIndex);
-        });
+//         document.getElementById('voltar').addEventListener('click', function() {
+//             currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
+//             updateCarousel(currentIndex);
+//         });
 
-        document.getElementById('avancar').addEventListener('click', function() {
-            currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
-            updateCarousel(currentIndex);
-        });
+//         document.getElementById('avancar').addEventListener('click', function() {
+//             currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
+//             updateCarousel(currentIndex);
+//         });
 
-        updateCarousel(currentIndex);
-    });
+//         updateCarousel(currentIndex);
+//     });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var buttons = document.querySelectorAll(".buy-btn");
-    buttons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            var item_name = this.closest(".product").querySelector("h2").innerText;
-            var customTitle = sanitizeString(item_name);
-            gtag('event', 'clique', {
-                'custom_section': 'view-produtos',
-                'custom_type': 'botao:comprar',
-                'custom_title': customTitle
-            });
-        });
-    });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     var buttons = document.querySelectorAll(".buy-btn");
+//     buttons.forEach(function(button) {
+//         button.addEventListener("click", function() {
+//             var item_name = this.closest(".product").querySelector("h2").innerText;
+//             var customTitle = sanitizeString(item_name);
+//             gtag('event', 'clique', {
+//                 'custom_section': 'view-produtos',
+//                 'custom_type': 'botao:comprar',
+//                 'custom_title': customTitle
+//             });
+//         });
+//     });
+// });
 
-    document.addEventListener("DOMContentLoaded", function() {
-        var links = document.querySelectorAll('nav ul li a');
+//     document.addEventListener("DOMContentLoaded", function() {
+//         var links = document.querySelectorAll('nav ul li a');
 
-        links.forEach(function(link) {
-            link.addEventListener("click", function(event) {
-            var nameNav = this.innerText;
-            var customTitle = sanitizeString(nameNav);
-                gtag('event', 'clique', {
-                    'custom_section': 'menu',
-                    'custom_type': 'botao-menu',
-                    'custom_title': customTitle
-                });
-            });
-        });
-    });
+//         links.forEach(function(link) {
+//             link.addEventListener("click", function(event) {
+//             var nameNav = this.innerText;
+//             var customTitle = sanitizeString(nameNav);
+//                 gtag('event', 'clique', {
+//                     'custom_section': 'menu',
+//                     'custom_type': 'botao-menu',
+//                     'custom_title': customTitle
+//                 });
+//             });
+//         });
+//     });
 
-document.getElementById("logo").querySelector('a').addEventListener("click", function () {
-    gtag('event', 'clique', {
-        'custom_section': 'menu',
-        'custom_type': 'icon',
-        'custom_title': 'logo'
-    });
-});
+// document.getElementById("logo").querySelector('a').addEventListener("click", function () {
+//     gtag('event', 'clique', {
+//         'custom_section': 'menu',
+//         'custom_type': 'icon',
+//         'custom_title': 'logo'
+//     });
+// });
 
 
-  document.addEventListener('DOMContentLoaded', function() {
+//   document.addEventListener('DOMContentLoaded', function() {
 
-    var linkedinLink = document.querySelector('a[href="https://www.linkedin.com/in/felipe-aur%C3%A9lio-silva-3a882528a/"]');
-    var githubLink = document.querySelector('a[href="https://github.com/feeags96"]');
+//     var linkedinLink = document.querySelector('a[href="https://www.linkedin.com/in/felipe-aur%C3%A9lio-silva-3a882528a/"]');
+//     var githubLink = document.querySelector('a[href="https://github.com/feeags96"]');
 
-    function MediaClickEvent(customTitle) {
-      gtag('event', 'clique', {
-        'custom_section': 'footer:icon',
-        'custom_type': 'icon-redes-sociais',
-        'custom_title': customTitle
-      });
-    }
+//     function MediaClickEvent(customTitle) {
+//       gtag('event', 'clique', {
+//         'custom_section': 'footer:icon',
+//         'custom_type': 'icon-redes-sociais',
+//         'custom_title': customTitle
+//       });
+//     }
 
  
-    if (linkedinLink) {
-      linkedinLink.addEventListener('click', function() {
-        MediaClickEvent('linkedin');
-      });
-    }
+//     if (linkedinLink) {
+//       linkedinLink.addEventListener('click', function() {
+//         MediaClickEvent('linkedin');
+//       });
+//     }
  
-    if (githubLink) {
-      githubLink.addEventListener('click', function() {
-        MediaClickEvent('github');
-      });
-    }
-  });
+//     if (githubLink) {
+//       githubLink.addEventListener('click', function() {
+//         MediaClickEvent('github');
+//       });
+//     }
+//   });
 
 
    
 
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var form = document.querySelector("#newsletter form");
-    var isFormInteracted = false;
-    var clickListener;
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var form = document.querySelector("#newsletter form");
+//     var isFormInteracted = false;
+//     var clickListener;
 
-    form.addEventListener("input", function() {
-        if (!isFormInteracted) {
-            isFormInteracted = true;
+//     form.addEventListener("input", function() {
+//         if (!isFormInteracted) {
+//             isFormInteracted = true;
 
-            clickListener = function(event) {
-                var isClickInsideForm = form.contains(event.target);
+//             clickListener = function(event) {
+//                 var isClickInsideForm = form.contains(event.target);
 
-                if (!isClickInsideForm) {
-                    gtag('event', 'abandono_form', {
-                        'custom_section': 'view-newsletter',
-                        'custom_type': 'interacao',
-                        'custom_title': 'newsletter_saiu'
-                    });
+//                 if (!isClickInsideForm) {
+//                     gtag('event', 'abandono_form', {
+//                         'custom_section': 'view-newsletter',
+//                         'custom_type': 'interacao',
+//                         'custom_title': 'newsletter_saiu'
+//                     });
 
-                    isFormInteracted = false;
+//                     isFormInteracted = false;
 
                 
-                    document.removeEventListener("click", clickListener);
-                }
-            };
+//                     document.removeEventListener("click", clickListener);
+//                 }
+//             };
 
-            document.addEventListener("click", clickListener);
-        }
-    });
-  });
+//             document.addEventListener("click", clickListener);
+//         }
+//     });
+//   });
 
-document.querySelector("#searchbtn button").addEventListener("click", function() {
-    var pesquisa = document.querySelector("#searchbtn input").value;
-    gtag('event', 'campo_search', {
-        'custom_section': 'menu',
-        'custom_type': 'search',
-        'custom_title': sanitizeString(pesquisa)
-    });
-});
+// document.querySelector("#searchbtn button").addEventListener("click", function() {
+//     var pesquisa = document.querySelector("#searchbtn input").value;
+//     gtag('event', 'campo_search', {
+//         'custom_section': 'menu',
+//         'custom_type': 'search',
+//         'custom_title': sanitizeString(pesquisa)
+//     });
+// });
 
 
 
